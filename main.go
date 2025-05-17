291427a58198a530b9938e9a33d5bad0b30acf17
Arfadhs <ariffadlilhasibuan@gmail.com>
2025-05-17 21:17:08 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var himpunan = [10]int{3, 23, 33, 64, 7, 26, 10, 88, 4, 5}
+
+	n := len(himpunan)
+
+	for i := 0; i < n; i++ {
+		j := i
+		temp := himpunan[i]
+		for j > 0 && temp < himpunan[j-1] {
+			himpunan[j] = himpunan[j-1]
+			j--
+		}
+		himpunan[j] = temp
+	}
+	fmt.Println(himpunan)
+}
